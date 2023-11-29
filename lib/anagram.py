a2045c121805173186d2f9e501c3eeb876bb27a1
calebmutisya <mutisyacalebmusau@gmail.com>
2023-11-29 15:55:21 +0300

    Completed

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,10 @@
 # your code goes here!
+class Anagram():
+    def __init__(self, word):
+        self.word=word 
+    
+    def match(self,list):
+        return [w for w in list if sorted(w.lower())== sorted(self.word.lower()) and w.lower() != self.word.lower()]
+    
+listen = Anagram("listen")
+print(listen.match(['enlists', 'google', 'inlets', 'banana']))
